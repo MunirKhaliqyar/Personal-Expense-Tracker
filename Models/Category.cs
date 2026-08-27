e8edff8b987400a97520c5a015cd035cfc8d7789
munir <munir.khaliyar@gmail.com>
2026-08-27 14:03:44 -0400

    Created a Category enum

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Expense_Tracker.Models
+{
+    // Category enumerator
+    internal enum Category
+    {
+        Food,
+        Transportation,
+        Utilities,
+        Entertainment,
+        Shopping,
+        Healthcare,
+        Rent,
+        Other
+    }
+}
